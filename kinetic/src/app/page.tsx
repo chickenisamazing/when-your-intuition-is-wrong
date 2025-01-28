0b595fdbc0475323a562e227b1d2612039f59098
chickenisamazing <dieswing@naver.com>
2025-01-28 16:11:15 +0900

    feat: 메인 페이지 테스트 수정

--- kinetic/src/app/page.tsx
+++ kinetic/src/app/page.tsx
@@ -6,21 +6,16 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/siroco.gif"
+          alt="siroco"
           width={180}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
+        <ol className="list-inside list-decimal text-xl text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            When your intuition is wrong
+          </code>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -30,27 +25,27 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
+            {/* <Image
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
-            />
-            Deploy now
+            /> */}
+            내 직관 시험하기
           </a>
-          <a
+          {/* <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             Read our docs
-          </a>
+          </a> */}
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -64,8 +59,8 @@ export default function Home() {
             height={16}
           />
           Learn
-        </a>
-        <a
+        </a> */}
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -79,7 +74,7 @@ export default function Home() {
             height={16}
           />
           Examples
-        </a>
+        </a> */}
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -93,7 +88,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          go to kea's pantry →
         </a>
       </footer>
     </div>
